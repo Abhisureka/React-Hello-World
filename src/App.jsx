4d0e0a437dd0edfc0ba8b71838686fd6a522ddf2
Abhishek <surekaabhi75@gmail.com>
2021-05-08 00:11:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function App() {
+
+    // let curDate = new Date();
+    // curDate = curDate.getHours();
+    // let greeting = '';
+    // let cssStlyle = {};
+
+    // if (curDate >= 1 && curDate < 12) {
+    //     greeting = 'Good Morning';
+    //     cssStlyle.color = "green";
+    // }
+    // else if (curDate >= 12 && curDate < 19) {
+    //     greeting = 'Good afternoon'
+    //     cssStlyle.color = "Orange";
+    // }
+    // else {
+    //     greeting = 'Good Night'
+    //     cssStlyle.color = "Blue";
+    // }
+
+    return (
+    <>
+        <div>
+            {/* <h1>Hello sir!! <span style={cssStlyle}> {greeting} </span>
+            </h1> */}
+            <h1>Hello world</h1>
+        </div>
+    </>
+);
+
+}
+
+export default App
